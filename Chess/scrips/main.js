7ba15e3b3291b3081cf3cea8f13c7fb124887611
Martin Raninger <m.raninger@students.htl-leonding.ac.at>
2023-06-11 20:58:29 +0200

    i added that the pieves r able to move now but im not quite done yet some things do still miss

--- Chess/scrips/main.js
+++ Chess/scrips/main.js
@@ -3,15 +3,28 @@ export const context = canvas.getContext("2d");
 
 import {Game} from "./chess.js";
 
+export let game;
 document.addEventListener('click', function(event) {
     let clickX = event.x;
     let clickY = event.y;
 });
 
 async function init() {
-    let game = new Game(600);
+    game = new Game(600);
     game.init();
 }
 $(async function () {
     await init()
 })
+
+canvas.addEventListener('click', function(event) {
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    // x and y are now relative to the canvas
+
+    let row = Math.floor(y / 75);
+    let col = Math.floor(x / 75);
+    console.log(col, row);
+    game.clickOn(col, row);
+});
