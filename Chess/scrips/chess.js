5be9c6a53b1683725eea87f3d8adf781690345d3
Martin Raninger <m.raninger@students.htl-leonding.ac.at>
2023-06-11 11:34:21 +0200

    project structure (classes)

--- Chess/scrips/chess.js
+++ Chess/scrips/chess.js
@@ -2,30 +2,61 @@ import {ChessClient} from "../../server/server-client.js";
 import {context} from "./main.js";
 
 export class Game {
-    constructor(size)
-    {
+    constructor(size) {
         this.client = new ChessClient();
         this.size = size;
         this.fieldSize = size / 8;
+        this.pieces = [
+            new Piece("black_rook", 0, 0),
+            new Piece("black_knight", 0, 1),
+            new Piece("black_bishop", 0, 2),
+            new Piece("black_queen", 0, 3),
+            new Piece("black_king", 0, 4),
+            new Piece("black_bishop", 0, 5),
+            new Piece("black_knight", 0, 6),
+            new Piece("black_rook", 0, 7),
+            new Piece("black_pawn", 1, 0),
+            new Piece("black_pawn", 1, 1),
+            new Piece("black_pawn", 1, 2),
+            new Piece("black_pawn", 1, 3),
+            new Piece("black_pawn", 1, 4),
+            new Piece("black_pawn", 1, 5),
+            new Piece("black_pawn", 1, 6),
+            new Piece("black_pawn", 1, 7),
+            new Piece("white_rook", 7, 0),
+            new Piece("white_knight", 7, 1),
+            new Piece("white_bishop", 7, 2),
+            new Piece("white_queen", 7, 3),
+            new Piece("white_king", 7, 4),
+            new Piece("white_bishop", 7, 5),
+            new Piece("white_knight", 7, 6),
+            new Piece("white_rook", 7, 7),
+            new Piece("white_pawn", 6, 0),
+            new Piece("white_pawn", 6, 1),
+            new Piece("white_pawn", 6, 2),
+            new Piece("white_pawn", 6, 3),
+            new Piece("white_pawn", 6, 4),
+            new Piece("white_pawn", 6, 5),
+            new Piece("white_pawn", 6, 6),
+            new Piece("white_pawn", 6, 7),
+        ];
     }
 
     init() {
-        this.initBoard();
-        this.initPieces();
+        this.drawBoard();
+        this.drawPieces();
     }
-    initBoard() {
+
+    drawBoard() {
         let row = 0;
         let col = 0;
         let isWhite = false;
 
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
-                if (isWhite)
-                {
+                if (isWhite) {
                     context.fillStyle = "#ffce99";
-                }
-                else
-                {
+                } else {
                     context.fillStyle = "#994f00";
                 }
 
@@ -35,51 +66,31 @@ export class Game {
             isWhite = !isWhite;
         }
     }
-    initPieces() {
-        const blackRook = new Image();
-        blackRook.src = "images/black_rook.png";
-        blackRook.onload = () => {
-            context.drawImage(blackRook, 0, 0, this.fieldSize, this.fieldSize)
-            context.drawImage(blackRook, this.fieldSize * 7, 0, this.fieldSize, this.fieldSize)
-        }
 
-        const blackKnight = new Image();
-        blackKnight.src = "images/black_knight.png";
-        blackKnight.onload = () => {
-            context.drawImage(blackKnight, this.fieldSize, 0, this.fieldSize, this.fieldSize)
-            context.drawImage(blackKnight, this.fieldSize * 6, 0, this.fieldSize, this.fieldSize)
+    drawPieces() {
+        for (const piece of this.pieces) {
+            let img = new Image();
+            img.src = `${piece.name}.png`;
+            context.drawImage(img, piece.x * this.fieldSize, piece.y * this.fieldSize);
         }
+    }
+}
 
-        const blackBishop = new Image();
-        blackBishop.src = "images/black_bishop.png";
-        blackBishop.onload = () => {
-            context.drawImage(blackBishop, this.fieldSize * 2, 0, this.fieldSize, this.fieldSize)
-            context.drawImage(blackBishop, this.fieldSize * 5, 0, this.fieldSize, this.fieldSize)
-        }
+export class Piece {
+    constructor(name, x, y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
 
-        const blackQueen = new Image();
-        blackQueen.src = "images/black_queen.png";
-        blackQueen.onload = () => {
-            context.drawImage(blackQueen, this.fieldSize * 3, 0, this.fieldSize, this.fieldSize)
-        }
+    move(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
-        const blackKing = new Image();
-        blackKing.src = "images/black_king.png";
-        blackKing.onload = () => {
-            context.drawImage(blackKing, this.fieldSize * 4, 0, this.fieldSize, this.fieldSize)
-        }
-
-        const blackPawn = new Image();
-        blackPawn.src = "images/black_pawn.png";
-        blackPawn.onload = () => {
-            context.drawImage(blackPawn, 0, this.fieldSize, this.fieldSize, this.fieldSize)
-            context.drawImage(blackPawn, this.fieldSize, this.fieldSize, this.fieldSize, this.fieldSize)
-            context.drawImage(blackPawn, this.fieldSize * 2, this.fieldSize, this.fieldSize, this.fieldSize)
-            context.drawImage(blackPawn, this.fieldSize * 3, this.fieldSize, this.fieldSize, this.fieldSize)
-            context.drawImage(blackPawn, this.fieldSize * 4, this.fieldSize, this.fieldSize, this.fieldSize)
-            context.drawImage(blackPawn, this.fieldSize * 5, this.fieldSize, this.fieldSize, this.fieldSize)
-            context.drawImage(blackPawn, this.fieldSize * 6, this.fieldSize, this.fieldSize, this.fieldSize)
-            context.drawImage(blackPawn, this.fieldSize * 7, this.fieldSize, this.fieldSize, this.fieldSize)
-        }
+export class Field {
+    constructor(size, x, y, piece) {
+        this.piece = piece;
     }
 }
